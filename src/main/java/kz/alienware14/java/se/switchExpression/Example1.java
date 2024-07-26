55f04012fda126175073a1e7ebe12350ac35c1b1
bagdarlamashy001 <bagdarlamashy@gmail.com>
2024-07-27 01:06:53 +0500

    Switch - Expression switch java +14

--- src/main/java/kz/alienware14/java/se/switchExpression/Example1.java
+++ src/main/java/kz/alienware14/java/se/switchExpression/Example1.java
@@ -3,10 +3,22 @@ package kz.alienware14.java.se.switchExpression;
 public class Example1 {
 
     public static void main(String[] args) {
+        // Expression switch java +14
+        System.out.println(expressionSwitchJava14(Day.FRIDAY));
         // Expression switch
         System.out.println(expressionSwitch(Day.FRIDAY));
         // Statement(Good Old C-style Switch)
         System.out.println(oldSwitch(Day.FRIDAY));
+    }
+
+    private static int expressionSwitchJava14(Day day){
+        // Expression switch java +14
+        return switch (day){
+            case MONDAY, FRIDAY, SUNDAY -> 6;
+            case TUESDAY -> 7;
+            case THURSDAY -> 8;
+            default-> 9;
+        };
     }
 
     private static int expressionSwitch(Day day){
